11368683cd5952869ad5f5d3b74bd47a6849ab4d
Christopher Johnson <cjohnson@performanceservices.com>
2019-12-20 17:03:08 -0500

    find non edge case adjacent mines

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import re
 
 class Cell:
     mine = None
-    mines = None
+    mines = 0
 
     def __init__(self, mine):
         self.mine = mine
@@ -25,6 +25,50 @@ class Game:
 
         # find adjacent mines
 
+        for y in tiles:
+            for x in tiles:
+
+                # perfect case
+
+                if x > 0 and x < self.tiles - 1 and y > 0 and y < self.tiles - 1:
+                    self.map[y][x].mines += self.map[y][x-1].mine
+                    self.map[y][x].mines += self.map[y][x+1].mine
+                    self.map[y][x].mines += self.map[y-1][x-1].mine
+                    self.map[y][x].mines += self.map[y-1][x].mine
+                    self.map[y][x].mines += self.map[y-1][x+1].mine
+                    self.map[y][x].mines += self.map[y+1][x-1].mine
+                    self.map[y][x].mines += self.map[y+1][x].mine
+                    self.map[y][x].mines += self.map[y+1][x+1].mine
+
+                # try: 
+                #     self.map[y][x].mines += self.map[y][x-1].mine
+                # except: pass
+                # try:
+                #     self.map[y][x].mines += self.map[y][x+1].mine
+                # except: pass
+                # try:
+                #     self.map[y][x].mines += self.map[y-1][x-1].mine
+                # except: pass
+                # try:
+                #     self.map[y][x].mines += self.map[y-1][x].mine
+                # except: pass
+                # try:
+                #     self.map[y][x].mines += self.map[y-1][x+1].mine
+                # except: pass
+                # try:
+                #     self.map[y][x].mines += self.map[y+1][x-1].mine
+                # except: pass
+                # try:
+                #     self.map[y][x].mines += self.map[y+1][x].mine
+                # except: pass
+                # try:
+                #     self.map[y][x].mines += self.map[y+1][x+1].mine
+                # except: pass
+
+
+    def debug(self, x, y):
+        print(self.map[y][x].mines)
+
     def clear(self, x, y):
         if self.map[y][x].mine:
             print("BOOM")
@@ -34,10 +78,14 @@ class Game:
         print("you flagged row {} and col {}".format(x,y))
 
     def do(self, user_input):
+        debug = 'd[0-9]+,[0-9]+'
         flag = 'f[0-9]+,[0-9]+'
         clear = '^[0-9]+,[0-9]+'
 
-        if re.match(flag, user_input):
+        if re.match(debug, user_input):
+            cmd = re.split('d|,', user_input)
+            self.debug(int(cmd[1]), int(cmd[2]))
+        elif re.match(flag, user_input):
             cmd = re.split('f|,', user_input)
             self.flag(int(cmd[1]), int(cmd[2]))
         elif re.match(clear, user_input):
@@ -66,7 +114,7 @@ class Game:
             for x in y:
                 if x.mine == True:
                     print('*', end="|")
-                else: print(end=" |")
+                else: print(x.mines, end="|")
             print()
 
 
