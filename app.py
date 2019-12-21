9ea36aa8a5ccfc912a3dbffef09b340d26c641e1
Christopher Johnson <cjohnson@performanceservices.com>
2019-12-21 13:34:57 -0500

    swap map for cells.. cause idiot

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Game:
 
     active = True
     tiles = 10
-    map = []
+    cells = []
 
     def get_mines(self, cell):
 
@@ -27,21 +27,21 @@ class Game:
         tiles = self.tiles - 1
 
         if x > 0:
-            cell.mines += self.map[y][x-1].mine
+            cell.mines += self.cells[y][x-1].mine
         if x < tiles:
-            cell.mines += self.map[y][x+1].mine
+            cell.mines += self.cells[y][x+1].mine
         if y > 0 and x > 0:
-            cell.mines += self.map[y-1][x-1].mine
+            cell.mines += self.cells[y-1][x-1].mine
         if y > 0:
-            cell.mines += self.map[y-1][x].mine
+            cell.mines += self.cells[y-1][x].mine
         if y > 0 and x < tiles:
-            cell.mines += self.map[y-1][x+1].mine
+            cell.mines += self.cells[y-1][x+1].mine
         if y < tiles and x > 0:
-            cell.mines += self.map[y+1][x-1].mine
+            cell.mines += self.cells[y+1][x-1].mine
         if y < tiles:
-            cell.mines += self.map[y+1][x].mine
+            cell.mines += self.cells[y+1][x].mine
         if y < tiles and x < tiles:
-            cell.mines += self.map[y+1][x+1].mine
+            cell.mines += self.cells[y+1][x+1].mine
 
     def setup(self):
         # create cells
@@ -52,10 +52,10 @@ class Game:
                 r = random.randint(0, 9)
                 row.append(Cell(True, (x, y)) if(
                     r > 8) else Cell(False, (x, y)))
-            self.map.append(row)
+            self.cells.append(row)
 
         # find adjacent mines
-        for y in self.map:
+        for y in self.cells:
             for cell in y:
                 self.get_mines(cell)
 
@@ -75,21 +75,21 @@ class Game:
                 y = cell.pos[1]
 
                 if x > 0:
-                    self.clear(self.map[y][x-1])
+                    self.clear(self.cells[y][x-1])
                 if x < tiles:
-                    self.clear(self.map[y][x+1])
+                    self.clear(self.cells[y][x+1])
                 if y > 0 and x > 0:
-                    self.clear(self.map[y-1][x-1])
+                    self.clear(self.cells[y-1][x-1])
                 if y > 0:
-                    self.clear(self.map[y-1][x])
+                    self.clear(self.cells[y-1][x])
                 if y > 0 and x < tiles:
-                    self.clear(self.map[y-1][x+1])
+                    self.clear(self.cells[y-1][x+1])
                 if y < tiles and x > 0:
-                    self.clear(self.map[y+1][x-1])
+                    self.clear(self.cells[y+1][x-1])
                 if y < tiles:
-                    self.clear(self.map[y+1][x])
+                    self.clear(self.cells[y+1][x])
                 if y < tiles and x < tiles:
-                    self.clear(self.map[y+1][x+1])
+                    self.clear(self.cells[y+1][x+1])
 
     def flag(self, cell):
         print("you flagged {}".format(cell.pos))
@@ -100,10 +100,10 @@ class Game:
 
         if re.match(flag, user_input):
             cmd = re.split('f|,', user_input)
-            self.flag(self.map[int(cmd[2])][int(cmd[1])])
+            self.flag(self.cells[int(cmd[2])][int(cmd[1])])
         elif re.match(clear, user_input):
             cmd = re.split(',', user_input)
-            self.clear(self.map[int(cmd[1])][int(cmd[0])])
+            self.clear(self.cells[int(cmd[1])][int(cmd[0])])
         elif user_input == 'exit':
             self.active = False
         else:
@@ -120,9 +120,9 @@ class Game:
             print(i, end=" ")
         print()
 
-        map = enumerate(self.map)
+        cells = enumerate(self.cells)
 
-        for iy, y in map:
+        for iy, y in cells:
             print(iy, end="|")
             for cell in y:
                 if cell.status == "cleared":
