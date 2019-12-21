2429c471d9c1fe4e0e81384a9229959e87992690
Christopher Johnson <cjohnson@performanceservices.com>
2019-12-21 12:48:20 -0500

    added display mechanism

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import random
 import re
 
+
 class Cell:
     pos = ()
     mine = None
     mines = 0
+    status = None
 
     def __init__(self, mine, pos):
         self.mine = mine
         self.pos = pos
 
+
 class Game:
+
     active = True
     tiles = 10
     map = []
@@ -22,15 +26,22 @@ class Game:
 
         tiles = self.tiles - 1
 
-        if x > 0: cell.mines += self.map[y][x-1].mine
-        if x < tiles: cell.mines += self.map[y][x+1].mine
-        if y > 0 and x > 0: cell.mines += self.map[y-1][x-1].mine
-        if y > 0: cell.mines += self.map[y-1][x].mine
-        if y > 0 and x < tiles: cell.mines += self.map[y-1][x+1].mine
-        if y < tiles and x > 0: cell.mines += self.map[y+1][x-1].mine
-        if y < tiles: cell.mines += self.map[y+1][x].mine
-        if y < tiles and x < tiles: cell.mines += self.map[y+1][x+1].mine
-
+        if x > 0:
+            cell.mines += self.map[y][x-1].mine
+        if x < tiles:
+            cell.mines += self.map[y][x+1].mine
+        if y > 0 and x > 0:
+            cell.mines += self.map[y-1][x-1].mine
+        if y > 0:
+            cell.mines += self.map[y-1][x].mine
+        if y > 0 and x < tiles:
+            cell.mines += self.map[y-1][x+1].mine
+        if y < tiles and x > 0:
+            cell.mines += self.map[y+1][x-1].mine
+        if y < tiles:
+            cell.mines += self.map[y+1][x].mine
+        if y < tiles and x < tiles:
+            cell.mines += self.map[y+1][x+1].mine
 
     def setup(self):
         # create cells
@@ -39,7 +50,8 @@ class Game:
             row = []
             for x in tiles:
                 r = random.randint(0, 9)
-                row.append(Cell(True, (x, y)) if(r>7) else Cell(False, (x, y)))
+                row.append(Cell(True, (x, y)) if(
+                    r > 7) else Cell(False, (x, y)))
             self.map.append(row)
 
         # find adjacent mines
@@ -47,41 +59,36 @@ class Game:
             for cell in y:
                 self.get_mines(cell)
 
-    def debug(self, x, y):
-        print(self.map[y][x].mines)
+    def clear(self, cell):
+        if cell.mine:
+            print("BOOM, GAME OVER")
+            self.active = False
+        else:
+            cell.status = "cleared"
 
-    def clear(self, x, y):
-        if self.map[y][x].mine:
-            print("BOOM")
-        else: print("nope :(")
-
-    def flag(self, x, y):
-        print("you flagged row {} and col {}".format(x,y))
+    def flag(self, cell):
+        print("you flagged {}".format(cell.pos))
 
     def do(self, user_input):
-        debug = 'd[0-9]+,[0-9]+'
         flag = 'f[0-9]+,[0-9]+'
         clear = '^[0-9]+,[0-9]+'
 
-        if re.match(debug, user_input):
-            cmd = re.split('d|,', user_input)
-            self.debug(int(cmd[1]), int(cmd[2]))
-        elif re.match(flag, user_input):
+        if re.match(flag, user_input):
             cmd = re.split('f|,', user_input)
-            self.flag(int(cmd[1]), int(cmd[2]))
+            self.flag(self.map[int(cmd[2])][int(cmd[1])])
         elif re.match(clear, user_input):
             cmd = re.split(',', user_input)
-            self.clear(int(cmd[0]), int(cmd[1]))
+            self.clear(self.map[int(cmd[1])][int(cmd[0])])
         elif user_input == 'exit':
             self.active = False
-        else: print("invalid move")
+        else:
+            print("invalid move")
 
     def user_input(self):
-        move = input('> ')
-        self.do(move)
-    
+        self.do(input('> '))
+
     def show(self):
-        
+
         # print header
         print(end="  ")
         for i in range(self.tiles):
@@ -90,20 +97,20 @@ class Game:
 
         map = enumerate(self.map)
 
-        for iy,y in map:
+        for iy, y in map:
             print(iy, end="|")
-            for x in y:
-                if x.mine == True:
-                    print('*', end="|")
-                else: print(x.mines, end="|")
+            for cell in y:
+                if cell.status == "cleared":
+                    if cell.mines > 0:
+                        print(cell.mines, end="|")
+                    else: print(" ", end="|")
+                else: print(".", end="|")
             print()
-
 
     def run(self):
         while self.active:
             self.show()
             self.user_input()
-
 
     def __init__(self):
         self.setup()
